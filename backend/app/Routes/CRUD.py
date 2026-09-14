cf723d2c0edafda57cc9be38444ac5c06e5e5337
usingBlender <enzomedeirossena@gmail.com>
2026-09-14 08:27:24 -0300

    fix: startup errors

--- backend/app/Routes/CRUD.py
+++ backend/app/Routes/CRUD.py
@@ -7,7 +7,6 @@ from sqlalchemy.orm import Session
 from Schemas.schemas import BookResponseSchema, CreateBookSchema, DeleteBookSchema, EditBookSchema
 from database.database import get_session
 
-from Models.models import Book
 from Repositories.static_storage import BookRepository
 
 CRUD_ROUTER = APIRouter()
