a667b3ad30d83657c7eb7c7d72cd74dc0c76a48a
andreary1 <andreary599@gmail.com>
2026-09-17 10:42:06 -0300

    feat: list schema and repository

---
+++ backend/app/Schemas/list_schema.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+from uuid import UUID
+
+from pydantic import BaseModel, ConfigDict, HttpUrl
+
+class CreateListSchema(BaseModel):
+    name: str
+
+class EditListSchema(BaseModel):
+    name: str | None
+
+class ListReponseSchema(CreateListSchema):
+    id: UUID
+    workspace_id: UUID
+    project_id: UUID
+    name: str
+    model_config = {
+        "from_attributes": True
+    }
+
+class DeleteListSchema(BaseModel):
+    id: UUID
+    mensagem: str
